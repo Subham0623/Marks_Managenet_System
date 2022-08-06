ad0571d0fc43c9bb73af6ba1093696b77435e9ce
bipinsu <np03cs4s210247@heraldcollege.edu.np>
2022-08-06 21:25:03 +0545

    response edited

--- js/response.js
+++ js/response.js
@@ -1,14 +1,13 @@
 function getBotResponse(input) {
     
     if (input == "BBA") {
-        return "Your request have been register is there anyother subject teacher u want to concerner";
+        return "Your request for BBA have been register is there anyother subject teacher u want to concerner";
     } else if (input == "BIT") {
-        return "Your request have been register is there anyother subject teacher u want to concerner";
+        return "Your request for BIT have been register is there anyother subject teacher u want to concerner";
     }
     //  else if (input == "Nepali") {
     //     return "Your request have been register is there anyother subject teacher u want to concerner";
     // }
-
     // Simple responses
     if (input == "hello") {
         return "Hello there!";
